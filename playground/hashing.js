abcf9e43baafa012593babfb79debf9101b420ef
Cody Farrey <codyfarrey@gmail.com>
2018-07-12 19:33:36 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 // const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$d2npbdsWYaM8Cj8l641KY.RgiqpY72p1Add6kQq7VuemqYRVeQ/yK';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
+
 // jwt.verify
 
 // var message = 'I am user number 3';
